0a5e5dd027438e291b3f35812e775b6d4c7871f8
Aden Collinge <aden.collinge@gmail.com>
2018-04-16 18:09:19 +0000

    fixed

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,7 +2,7 @@ var getHTML = require('./https-functions.js');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step5.html'
+  path: '/http-examples/step6/reverse.html'
 };
 
 
