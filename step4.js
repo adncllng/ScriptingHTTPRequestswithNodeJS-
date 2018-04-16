bc08c31209b1e7dbb72cf288e112eaf09a3f1b8e
Aden Collinge <aden.collinge@gmail.com>
2018-04-16 17:26:11 +0000

    step 4

---
+++ step4.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+function getHTML(options, callback) {
+
+  https.get(options, (res) => {
+    let html = '';
+    res.setEncoding('utf8');
+    res.on('data', (data) => {
+      html += data;
+      res.on('end', () => {
+        callback(html);
+      });
+    });
+  });
+}
+
+function printHTML(html) {
+  console.log(html);
+}
+
+
+let requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
